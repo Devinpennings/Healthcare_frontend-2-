fcdb02f89a6f708292ac32b3107e45dcf7d3d090
Davey vd Bogaard <daveyvdbogaard@live.nl>
2018-05-30 16:48:57 +0200

    postcodeLookup added

--- Healthcare/src/components/dashboard/CreateM.vue
+++ Healthcare/src/components/dashboard/CreateM.vue
@@ -55,16 +55,16 @@
               <div class="col-sm-10">
                 <div class="row">
                   <div class="col-md-4">
-                    <input type="text" placeholder="Plaats" v-model="city" v-on:keyup="checkForm" class="form-control">
+                    <input type="text" placeholder="Postcode" v-model="zipcode" v-on:keyup="checkForm, getAddress()" class="form-control">
                   </div>
                   <div class="col-md-4">
-                    <input type="text" placeholder="Straat" v-model="street" v-on:keyup="checkForm" class="form-control">
-                  </div>
-                  <div class="col-md-3">
-                    <input type="number" placeholder="Huisnummer" v-model="housenumber" v-on:keyup="checkForm" class="form-control">
+                  <input type="number" placeholder="Huisnummer" v-model="housenumber" v-on:keyup="checkForm, getAddress()" class="form-control">
+                </div>
+                  <div class="col-md-4">
+                    <input type="text" placeholder="Plaats" v-model="city" v-on:keyup="checkForm" class="form-control" disabled>
                   </div>
                   <div class="col-md-4">
-                    <input type="text" placeholder="Postcode" v-model="zipcode" v-on:keyup="checkForm" class="form-control">
+                    <input type="text" placeholder="Straat" v-model="street" v-on:keyup="checkForm" class="form-control" disabled>
                   </div>
                 </div>
               </div>
@@ -90,6 +90,8 @@
 <script>
 
     import Datepicker from "../Datepicker"
+
+    let request = require("request");
 
     export default {
       name: "createm",
@@ -169,7 +171,7 @@
         },
         checkForm:function(e) {
           this.errors = [];
-          console.log(this.firstname);
+          console.log("straat= " + this.street)
           if(!this.email || !this.name || !this.lname || !this.birthdate || !this.street || !this.housenumber || !this.city || !this.zipcode || !this.beroep) {
             this.errors.push("Alle velden moeten ingevoerd worden");
           } else if(!this.validEmail(this.email)) {
@@ -178,6 +180,29 @@
           if(!this.errors.length) return true;
           e.preventDefault();
 
+        },
+        getAddress(){
+          if (this.zipcode === '' || this.housenumber === ''){
+            return null
+          } else {
+            this.$store.dispatch("addressAPI", 'addresses/?postcode=' + this.zipcode + '&number=' + this.housenumber).then(body => {
+              this.city = body._embedded.addresses[0].city.label;
+              this.street = body._embedded.addresses[0].street;
+            });
+            // let options = { method: 'GET',
+            //   url: 'https://api.postcodeapi.nu/v2/addresses/?postcode=' + this.zipcode + '&number=' + this.housenumber,
+            //   headers:
+            //     { accept: 'application/hal+json',
+            //       'x-api-key': '4sTCDMOqb8ayY3EAScQWO7F1ZmKjQNIhYXP6RlMg' } };
+            // request(options, function (error, response, body) {
+            //   if (error) throw new Error(error);
+            //   let data = JSON.parse(body);
+            //   this.city = data._embedded.addresses[0].city.label;
+            //   this.street = data._embedded.addresses[0].street;
+            //   console.log(city + street)
+            // });
+            // console.log(city + street)
+          }
         },
         validEmail:function(email) {
           let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
